3d6df0f8c128c46e42d9ed1dfde9cfd7ba23f129
Farzad <farzad.zare@live.com>
2019-08-27 12:38:26 +0430

    Farzad

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/************************************************************************
+*                                                                       *
+*     Created by Farzad Zare                                            *
+*     Date : 2019-08-27                                                 *
+*     Email : farzad.zare@live.com                                      *
+*     github : https://github.com/FarzadMarius/snackbar-react-native    *
+*                                                                       *
+*************************************************************************/
+
 import SnackBar from "./src/SnackBar";
 
 export default SnackBar;
